ed46cbffcb84081b697e6e4d832d3a6a874ff9ff
psinghcan <psinghcan@gmail.com>
2021-08-19 11:31:30 -0400

    Initial commit

---
+++ src/main/java/com/psinghcan/demobaseapp/DemoBaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.psinghcan.demobaseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBaseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoBaseAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psinghcan/demobaseapp/DemoBaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psinghcan.demobaseapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBaseAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
